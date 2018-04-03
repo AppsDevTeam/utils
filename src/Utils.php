c41b0d65a004301173a78751c97bc6c4acafdf41
honza@appsdevteam.com <gagaga159>
2018-04-03 14:05:02 +0200

    Metoda handleGuzzleError

---
+++ src/Utils.php
@@ -0,0 +1,30 @@
+<?php
+namespace ADT\Utils;
+
+class Utils {
+
+	/**
+	 * Returns TRUE if everything is allright, FALSE if it's repetable error, otherwise throws exception
+	 *
+	 * \GuzzleHttp\Exception\GuzzleException $guzzleException
+	 * @return boolean|\GuzzleHttp\Exception\GuzzleException
+	 */
+	public static function handleGuzzleError(\GuzzleHttp\Exception\GuzzleException $guzzleException) {
+
+		if ($guzzleException instanceof \GuzzleHttp\Exception\ConnectException) {
+			// HTTP Code 0
+			// On Sparkpost or ADT MailApi the request was successfuly processed even if there is no response
+			// Let's believe it is common behaviour
+			return TRUE;
+		}
+
+		if ($guzzleException instanceof \GuzzleHttp\Exception\ServerException) {
+			// HTTP Code 5xx
+			return FALSE;
+		}
+
+		// other exceptions like 3xx (TooManyRedirectsException) or 4xx (\GuzzleHttp\Exception\ClientException) are unrepeatable and we want to throw exception
+		throw $guzzleException;
+	}
+
+}
