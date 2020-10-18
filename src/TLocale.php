84aec74f88b52930123582d9624818cd3be0ddf0
Tomáš Kudělka <thorewi@gmail.com>
2020-10-18 20:09:41 +0200

    Create TLocale.php

---
+++ src/TLocale.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace ADT\Utils;
+
+use Nette\Localization\ITranslator;
+
+trait TLocale
+{
+	/** @persistent */
+	public ?string $locale;
+
+	/** @var ITranslator @autowire */
+	protected ?ITranslator $translator;
+
+	public function injectLocale()
+	{
+		$this->onStartup[] = function($xxx) {
+			$locale = $this->locale;
+
+			// uzivatel se prepl na jinou lokalizaci pomoci odkazu na strance
+			// tuto lokalizaci mu nastavime do cookie, abychom ho odted vzdy presmerovali na tuto lokalizaci,
+			// pokud prijde na url, ze ktere neni jasna lokalizace
+			if ($this->getParameter('switchToLocale')) {
+				$locale = $this->getParameter('switchToLocale');
+
+				setcookie('locale', $locale, time() + (3600 * 24 * 14), '/');
+			}
+			// pokud nelze lokalizaci urcit z url adresy,
+			// pokusime se nejvhodnejsi urcit z COOKIE nebo z jazyka prohlizece,
+			// a pripadne na ni presmerovat
+			elseif ($locale === null || $locale === $this->translator->getDefaultLocale()) {
+				// pokud uzivatel dosel na stranky poprve, pokusime se zjistit jazyk prohlizece
+				// a podle nej vybrat nejvhodnejsi locale
+				if (empty($_COOKIE['locale'])) {
+					$lang = substr($this->getHttpRequest()->getHeader('Accept-Language'), 0, 2);
+
+					// pokud jsou stranky lokalizovane do jazyka prohlizece,
+					// nastavime tuto lokalizaci
+					if (in_array($lang, $this->translator->getAvailableLocales())) {
+						$locale = $lang;
+					}
+					// pokud jazyk prohlizece je slovenstina a stranky jsou lokalizovane do cestiny,
+					// nastavime cestinu
+					elseif ($lang === 'sk' && in_array('cs', $this->translator->getAvailableLocales())) {
+						$locale = 'cs';
+					}
+					// pokud jazyk prohlizece je cestina a stranky jsou lokalizovane do slovenstiny,
+					// nastavime slovenstinu
+					elseif ($lang === 'cs' && in_array('sk', $this->translator->getAvailableLocales())) {
+						$locale = 'cs';
+					}
+					// pokud nemame stranky lokalizovane do jazyka prohlizece, ale mame je lokalizovane do anglictiny
+					// nastavime anglictinu
+					elseif (in_array('en', $this->translator->getAvailableLocales())) {
+						$locale = 'en';
+					}
+					// pokud neni nastaven jazyk prohlizece (napriklad u robota) a neni nastavena zadna vychozi lokalizace,
+					// nastavime prvni fallback lokalizaci
+					elseif ($locale === null) {
+						$locale = $this->translator->getFallbackLocales()[0];
+					}
+
+					setcookie('locale', $locale, time() + (3600 * 24 * 14), '/');
+				} else {
+					$locale = $_COOKIE['locale'];
+				}
+			}
+
+			// pokud jsme urcili ze nejsme na nejvhodnejsi lokalizaci,
+			// presmerujeme
+			if ($locale !== $this->locale) {
+				$this->redirect('this', ['locale' => $locale]);
+			}
+
+			$this->locale = $locale;
+		};
+
+		$this->onRender[] = function() {
+			$this->template->locale = $this->locale;
+		};
+	}
+}
