fc69e752b4a0099d93e87b7296853ce3a711d6f3
Tomáš Kudělka <thorewi@gmail.com>
2020-10-31 17:16:44 +0200

    Update Route.php

--- src/Route.php
+++ src/Route.php
@@ -2,6 +2,7 @@
 
 namespace ADT\Utils;
 
+use Nette\Http\Request;
 use Nette\Http\Url;
 use Nette\Http\UrlScript;
 use Nette\Localization\ITranslator;
@@ -17,8 +18,12 @@ interface IRouteFactory
  */
 class Route extends \Nette\Application\Routers\Route
 {
-	public function __construct(string $mask, $metadata = [], int $flags = 0, $disableLocaleParameter = false, ?ITranslator $translator = null)
+	protected Request $httpRequest;
+
+	public function __construct(string $mask, $metadata = [], int $flags = 0, $disableLocaleParameter = false, ?ITranslator $translator = null, Request $httpRequest)
 	{
+		$this->httpRequest = $httpRequest;
+
 		$locale = '';
 		if (!$disableLocaleParameter && $translator) {
 			$locale = '[<locale' . ($translator->getDefaultLocale() ? '=' . $translator->getDefaultLocale() : '') . ' ' . implode('|', $translator->getAvailableLocales()) . '>/]';
@@ -34,6 +39,12 @@ class Route extends \Nette\Application\Routers\Route
 	public function constructUrl(array $params, UrlScript $refUrl): ?string
 	{
 		$url = parent::constructUrl($params, $refUrl);
+
+		if ($params['presenter'] === 'Error') {
+			$nurl = new Url($url);
+			$nurl->setPath($this->httpRequest->getUrl()->getPath());
+			$url = $nurl->getAbsoluteUrl();
+		}
 
 		if ($url !== null && !in_array($refUrl->getPort(), Url::$defaultPorts, true)) {
 			$nurl = new Url($url);
