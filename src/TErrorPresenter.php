af1571e3558c00bda2bdcea6d39d59c2e21e8ea5
Tomáš Kudělka <thorewi@gmail.com>
2020-11-01 13:58:08 +0200

    Update TErrorPresenter.php

--- src/TErrorPresenter.php
+++ src/TErrorPresenter.php
@@ -4,8 +4,10 @@ namespace Adt\Utils;
 
 use ADT\Utils\IRouteFactory;
 use App\Modules\WebModule\BasePresenter;
+use Nette\Application\Application;
 use Nette\Application\BadRequestException;
 use Nette\Application\Helpers;
+use Nette\Http\Response;
 use Nette\Routing\Router;
 use Tracy\Debugger;
 use Tracy\ILogger;
@@ -13,6 +15,8 @@ use Tracy\ILogger;
 trait TErrorPresenter
 {
 	protected $exception;
+	
+	protected bool $log404 = false;
 
 	public function __construct(Router $router, IRouteFactory $routeFactory)
 	{
@@ -21,16 +25,34 @@ trait TErrorPresenter
 		$this->onStartup[] = function() use ($router, $routeFactory) {
 			$this->exception = $this->getRequest()->getParameter('exception');
 
-			if ($this->exception instanceof BadRequestException) {
-				[$moduleName, $presenterName] = Helpers::splitName($this->getName());
-				foreach ($router->getRouters() as $routeList) {
-					if ($routeList->getModule() === $moduleName . ':') {
-						$routeList[] = $route = $routeFactory->create('<url .+>', $presenterName . ':' . $this->getAction());
-						break;
-					}
-				}
+			// nemusi existovat zadna routa odpovidajici zadane url
+			// abychom mohli pouzivat $this->link('this'), musime vytvorit routu, ktera matchne zadanou url
+			[$moduleName, $presenterName] = Helpers::splitName($this->getName());
+			foreach ($router->getRouters() as $routeList) {
+				if ($routeList->getModule() === $moduleName . ':') {
+					// vytvorime routu v presnem zneni soucasne url adresy
+					$url = $this->getHttpRequest()->getUrl()->getPath();
+					$route = $routeFactory->create('<url=' . $this->getHttpRequest()->getUrl()->getPath() . ' .*>', $presenterName . ':' . $this->getAction());
 
-				$this->loadState($route->match($this->getHttpRequest()));
+					// je potreba novou routu umistit na zacatek, aby se nam pouzila pri constructUrl
+					$routeList->prepend($route);
+
+					break;
+				}
+			}
+
+			$params = $route->match($this->getHttpRequest());
+
+			// je potreba, aby fungovaly persistentni parametry, napriklad "locale"
+			$this->loadState($params);
+
+			// BadRequst muze mit bud kod 404 (neexistuji stranka) nebo 403 (neexistujici handle)
+			if ($this->exception instanceof BadRequestException) {
+				// je potreba resit rucne, protoze vyhodnocovani signalu probehlo jeste pred FORWARDovanim do ErrorPresenteru
+				// v ErrorPresenteru uz se nic nevyhodnocuje
+				if (isset($params[static::SIGNAL_KEY]) && $params['do'] === '404') {
+					$this->handle404();
+				}
 			} 
 			else {
 				Debugger::log($this->exception, ILogger::EXCEPTION);
@@ -38,8 +60,21 @@ trait TErrorPresenter
 		};
 	}
 
-	public function renderDefault(string $url): void
+	public function handle404()
 	{
-		
+		Debugger::log('Error 404 with referer ' . $this->getHttpRequest()->getReferer() . ' (' . $_SERVER['HTTP_USER_AGENT'] . '; ' . $_SERVER['REMOTE_ADDR'] . ')', '404');
+		die();
+	}
+	
+	public function onShutdown()
+	{
+		if ($this->log404) {
+			register_shutdown_function(function () {
+				echo "<script>" . PHP_EOL;
+				require __DIR__ . '/assets/bot-detector.js';
+				echo "new BotDetector({ callback: function(result) { if (!result.isBot) navigator.sendBeacon('" . $this->link('404!') . "'); } }).monitor();" . PHP_EOL;
+				echo "</script>";
+			});
+		}
 	}
 }
