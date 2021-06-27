3b0156759f69e88f7f58f6518198d6eaa3877b4e
Tomáš Kudělka <thorewi@gmail.com>
2021-06-27 10:23:50 +0200

    Create EntityTrait.php

---
+++ src/Translatable/EntityTrait.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ADT\Utils\Translatable;
+
+trait EntityTrait
+{
+	/**
+	 * @Gedmo\Locale
+	 * Used locale to override Translation listener`s locale
+	 * this is not a mapped field of entity metadata, just a simple property
+	 */
+	private $locale;
+
+	public function setTranslatableLocale($locale)
+	{
+		$this->locale = $locale;
+		return $this;
+	}
+}
