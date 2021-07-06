337d4423cecfe381a0cc3642f9ccdf97391fa6fb
Tomáš Kudělka <thorewi@gmail.com>
2021-07-06 10:27:25 +0200

    Create TranslatableSluggableTrait.php

---
+++ src/Translatable/TranslatableSluggableTrait.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace ADT\Utils\Translatable;
+
+use ADT\DoctrineForms\Entity;
+use ADT\DoctrineForms\EntityFormMapper;
+use ADT\DoctrineForms\Form;
+use ADT\Forms\DynamicContainer;
+use App\Components\Forms\Base\EntityForm;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
+use Gedmo\Sluggable\SluggableListener;
+use Gedmo\Translatable\Entity\Repository\TranslationRepository;
+use Nette\Forms\Controls\BaseControl;
+
+trait TranslatableSluggableTrait
+{
+	abstract public function getForm(): EntityForm;
+	
+	abstract protected function getSluggableListener(): SluggableListener;
+
+	/**
+	 * @param TranslatableEntityInterface $entity
+	 * @param string $sourceField
+	 * @param string $field
+	 * @throws NonUniqueResultException
+	 */
+	private function generateSlug(TranslatableEntityInterface $entity, string $sourceField = 'name', string $field = 'slug')
+	{
+		$form = $this->getForm();
+
+		/** @var TranslationRepository $repository */
+		$repository = $form->getEntityManager()->getRepository('Gedmo\Translatable\Entity\Translation');
+
+		foreach ($repository->findTranslations($entity) as $_locale => $_translation) {
+			$slug = $_translation[$sourceField];
+			$slug = $this->getSluggableListener()->getTransliterator()($slug);
+			$slug = $this->getSluggableListener()->getUrlizer()($slug);
+
+			$originalSlug = $slug;
+			$counter = 1;
+			while (true) {
+				try {
+					$repository->createQueryBuilder('e')
+						->where('e.locale = :locale')
+						->andWhere('e.objectClass = :objectClass')
+						->andWhere('e.content = :content')
+						->andWhere('e.foreignKey != :foreignKey')
+						->andWhere('e.field = :field')
+						->setParameters([
+							'locale' => $_locale,
+							'objectClass' => get_class($entity),
+							'content' => $slug,
+							'foreignKey' => $entity->getId(),
+							'field' => $field,
+						])
+						->getQuery()
+						->getSingleResult();
+
+					$slug = $originalSlug . '-' . $counter++;
+				} catch (NoResultException $e) {
+					break;
+				}
+			}
+			$repository->translate($entity, $field, $_locale, $slug);
+		}
+	}
+}
