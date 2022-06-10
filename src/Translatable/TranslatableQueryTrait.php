0d14778d933ec84f8df76b28974e72a886d6d8cf
Marek Valuch <marek@appsdevteam.com>
2022-06-10 11:53:26 +0200

    refactorization

--- src/Translatable/TranslatableQueryTrait.php
+++ src/Translatable/TranslatableQueryTrait.php
@@ -37,52 +37,20 @@ trait TranslatableQueryTrait
 		return $this;
 	}
 
-	protected function getQuery(EntityRepository $repository)
+	private function getHints(): array
 	{
-		return $this->applyHints(parent::getQuery($repository));
+		$hints = [];
 
-
-	}
-
-	public function count(EntityRepository $repository = null, ResultSet $resultSet = null, Paginator $paginatedQuery = null)
-	{
-		if (is_null($repository)) {
-			$repository = $this->em->getRepository($this->getEntityClass());
-		}
-
-		if ($queryBuilder = $this->doCreateCountQuery($repository)) {
-			return $this->applyHints($queryBuilder->getQuery())->getSingleScalarResult();
-		}
-
-		return parent::count($repository, $resultSet, $paginatedQuery);
-	}
-
-	private function applyHints(Query $query): Query
-	{
 		if (is_a($this->getEntityClass(), TranslatableEntityInterface::class, true)) {
-			$query->setHint(
-				Query::HINT_CUSTOM_OUTPUT_WALKER,
-				'Gedmo\\Translatable\\Query\\TreeWalker\\TranslationWalker'
-			);
-
-			$query->setHint(
-				TranslatableListener::HINT_TRANSLATABLE_LOCALE,
-				$this->locale ?: $this->translator->getLocale()
-			);
-
-			$query->setHint(
-				TranslatableListener::HINT_FALLBACK,
-				true
-			);
+			$hints[Query::HINT_CUSTOM_OUTPUT_WALKER] = 'Gedmo\\Translatable\\Query\\TreeWalker\\TranslationWalker';
+			$hints[TranslatableListener::HINT_TRANSLATABLE_LOCALE] = $this->locale ?: $this->translator->getLocale();
+			$hints[TranslatableListener::HINT_FALLBACK] = true;
 
 			if (!$this->defaultLocaleFallback) {
-				$query->setHint(
-					TranslatableListener::HINT_INNER_JOIN,
-					true
-				);
+				$hints[TranslatableListener::HINT_INNER_JOIN] = true;
 			}
 		}
 
-		return $query;
+		return $hints;
 	}
 }
