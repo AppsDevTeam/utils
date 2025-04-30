f1c9c02cefe0ed44392151a59f9534f27312e463
Tomáš Kudělka <tomas@appsdevteam.com>
2025-04-30 09:33:48 +0200

    improvements

--- src/Filters/Image.php
+++ src/Filters/Image.php
@@ -6,7 +6,6 @@ namespace ADT\Utils\Filters;
 use ADT\Utils\FileSystem;
 use Exception;
 use Nette\Utils\ImageException;
-use Nette\Utils\UnknownImageFileException;
 
 class Image
 {
@@ -25,12 +24,14 @@ class Image
 		IMAGETYPE_WEBP => 'webp'
 	];
 
+	const ExtensionsToFormat = [
+		'webp' => IMAGETYPE_WEBP
+	];
+
 	/**
-	 * @throws ImageException
-	 * @throws UnknownImageFileException
 	 * @throws Exception
 	 */
-	public function format(string $url, int $width, int $height, int $mode = \Nette\Utils\Image::FIT, int $format = IMAGETYPE_WEBP): string
+	public function format(string $url, int $width, int $height, int $mode = \Nette\Utils\Image::OrSmaller, int $format = IMAGETYPE_WEBP): string
 	{
 		$isRemoteUrl = $this->isRemoteUrl($url);
 
@@ -45,46 +46,22 @@ class Image
 
 		} else {
 			$url = trim($url, '/');
-
 			if (!file_exists($this->path . '/' . $url))  {
 				return $url;
 			}
+			$contents = file_get_contents($this->path . '/' . $url);
 
 			list($urlWithoutExtension,) = $this->splitUrlOnLastDot($url);
 		}
 
-
-
 		$width = $width * $this->multiplier;
 		$height = $height * $this->multiplier;
-		$newPath = $this->path . '/' . $this->dir;
-		$newFileName = $urlWithoutExtension . '_' . $width . '_' . $height . '_' . $mode .  '.' . self::FormatToExtensions[$format];
+		$ext = pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_EXTENSION);
+		$newFile =  $this->dir . '/' . $urlWithoutExtension . '_' . $width . '_' . $height . '_' . $mode .  '_' . $ext . '.' . self::FormatToExtensions[$format];
 
-		// thumbnail already exists
-		if (file_exists($newPath . '/' . $newFileName)) {
-			goto end;
-		}
+		$this->createImage($contents, $width, $height, $mode, $format, $this->path . '/' . $newFile);
 
-		FileSystem::createDirAtomically(dirname($newPath . '/' . $newFileName));
-
-		$prevErrorHandler = set_error_handler(function ($errno, $errstr) use (&$prevErrorHandler) {
-			if ($errno === E_USER_WARNING && $errstr === 'Nette\Utils\Image::fromFile(): gd-png: libpng warning: iCCP: known incorrect sRGB profile') {
-				return true;
-			}
-			return $prevErrorHandler ? $prevErrorHandler(...func_get_args()) : false;
-		});
-		if ($isRemoteUrl) {
-			$image = \Nette\Utils\Image::fromString($contents);
-		} else {
-			$image = \Nette\Utils\Image::fromFile($this->path . '/' . $url);
-		}
-		set_error_handler($prevErrorHandler);
-		$image->resize($width, $height, $mode);
-		$image->save($newPath . '/' . $newFileName, 100, $format);
-
-		end:
-
-		return '/' . $this->dir . '/' . $newFileName;
+		return $newFile;
 	}
 
 	private function isRemoteUrl(string $url): bool
@@ -114,5 +91,53 @@ class Image
 	private function removeProtocol(string $url): string
 	{
 		return preg_replace("/^https?:\/\//", "", $url);
+	}
+
+	/**
+	 * @throws ImageException
+	 */
+	public function createImageFromThumbnailUrl(string $url): void
+	{
+		$info = pathinfo($url);
+		$format = $info['extension'];
+		$fileinfo = pathinfo($info['filename']);
+		$segments = explode('_', $fileinfo['filename']);
+		$extension = array_pop($segments);
+		$mode = (int)array_pop($segments);
+		$height = (int)array_pop($segments);
+		$width = (int)array_pop($segments);
+		$originalFile = $info['dirname'] . '/' . implode('_', $segments) . '.' . $extension;
+
+		$this->createImage(file_get_contents($this->path . '/' . $originalFile), $width, $height, $mode, static::ExtensionsToFormat[$format], $this->path . '/' . $this->dir . '/' . $url);
+	}
+
+	/**
+	 * @throws ImageException
+	 * @throws Exception
+	 */
+	protected function createImage(string $contents, int $width, int $height, int $mode, int $format, string $newFile): void
+	{
+		// thumbnail already exists
+		if (file_exists($newFile)) {
+			return;
+		}
+
+		FileSystem::createDirAtomically(dirname($newFile));
+
+		$prevErrorHandler = set_error_handler(function ($errno, $errstr) use (&$prevErrorHandler) {
+			if ($errno === E_USER_WARNING && $errstr === 'Nette\Utils\Image::fromString(): gd-png: libpng warning: iCCP: known incorrect sRGB profile') {
+				return true;
+			}
+			return $prevErrorHandler ? $prevErrorHandler(...func_get_args()) : false;
+		});
+		$image = \Nette\Utils\Image::fromString($contents);
+		set_error_handler($prevErrorHandler);
+		$image->resize($width, $height, $mode);
+		$image->save($newFile, 100, $format);
+	}
+
+	public function getPath(): string
+	{
+		return $this->path;
 	}
 }
