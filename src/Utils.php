f7f017213a61928e3de83711ac41539c8e7d689f
Tomáš Kudělka <thorewi@gmail.com>
2025-08-26 07:30:34 +0200

    Update Utils.php

--- src/Utils.php
+++ src/Utils.php
@@ -13,20 +13,20 @@ class Utils
 		return empty($value) && $value !== 0 && $value !== '0';
 	}
 
-	final public static function getDateTimeFromArray(array|string $data): ?DateTimeImmutable
+	final public static function getDateTimeFromArray(mixed $data, bool $returnOriginalOnError = false)
 	{
 		if (!isset($data['date'], $data['timezone'], $data['timezone_type'])) {
-			return null;
+			return $returnOriginalOnError ? $data : null;
 		}
 
 		if (!is_string($data['date']) || !is_string($data['timezone']) || !is_int($data['timezone_type'])) {
-			return null;
+			return $returnOriginalOnError ? $data : null;
 		}
 
 		try {
 			return new DateTimeImmutable($data['date'], new DateTimeZone($data['timezone']));
 		} catch (Exception) {
-			return null;
+			return $returnOriginalOnError ? $data : null;
 		}
 	}
 }
